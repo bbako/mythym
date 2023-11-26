5f3b6f1b03734aee5d62543262d05e6b3586db19
young <hoyeon1008@gmail.com>
2023-11-26 21:48:13 +0900

    1st

---
+++ mythym/src/test/java/com/example/mythym/MythymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mythym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
